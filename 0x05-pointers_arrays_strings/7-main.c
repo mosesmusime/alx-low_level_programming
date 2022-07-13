aca503dbf4e2feaad73f9551526b866dcf978c42
mosesmusime <mosesmusime@gmail.com>
2022-07-13 13:53:44 -0700

    A function that prints half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -check the code
+ * main - check the code
  *
  * Return: 0
  */
@@ -10,6 +10,6 @@ int main(void)
 	char *str;
 
 	str = "0123456789";
-	puts2(str);
+	puts_half(str);
 	return (0);
 }
